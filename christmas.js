f355c85036477b703633fa8c99b7c3241001f924
missesmashu <hansontm@uwec.edu>
2022-12-07 09:02:10 -0600

    javascript update

--- christmas.js
+++ christmas.js
@@ -1,22 +1,33 @@
+
 document.addEventListener("DOMContentLoaded", function(){
-	document.querySelector('.christmascountdown p:first-child').addEventListener('click', function(){
-		document.querySelector('.gift.one').classList.toggle('show');
-		document.querySelector('.overlay').classList.toggle('show');
+	document.querySelector('.christmascountdown>p:first-child').addEventListener('click', function(){
+		document.querySelector('.gift.one').classList.add('show');
+		document.querySelector('.overlay').classList.add('show');
 	});
-	document.querySelector('.christmascountdown p:nth-child(5)').addEventListener('click', function(){
-		document.querySelector('.gift.five').classList.toggle('show');
-		document.querySelector('.overlay').classList.toggle('show');
+	document.querySelector('.christmascountdown>p:nth-child(5)').addEventListener('click', function(){
+		document.querySelector('.gift.five').classList.add('show');
+		document.querySelector('.overlay').classList.add('show');
 	});
-	document.querySelector('.christmascountdown p:nth-child(6)').addEventListener('click', function(){
-		document.querySelector('.gift.six').classList.toggle('show');
-		document.querySelector('.overlay').classList.toggle('show');
+	document.querySelector('.christmascountdown>p:nth-child(6)').addEventListener('click', function(){
+		document.querySelector('.gift.six').classList.add('show');
+		document.querySelector('.overlay').classList.add('show');
 	});
-	document.querySelector('.exit').addEventListener('click', function(){
-		document.querySelector('.gift').classList.toggle('show');
-		document.querySelector('.overlay').classList.toggle('show');
+	document.querySelector('.gift.one .exit').addEventListener('click', function(){
+		document.querySelector('.gift').classList.remove('show');
+		document.querySelector('.overlay').classList.remove('show');
+	});
+	document.querySelector('.gift.five .exit').addEventListener('click', function(){
+		document.querySelector('.gift.five').classList.remove('show');
+		document.querySelector('.overlay').classList.remove('show');
+	});
+	document.querySelector('.gift.six .exit').addEventListener('click', function(){
+		document.querySelector('.gift.six').classList.remove('show');
+		document.querySelector('.overlay').classList.remove('show');
 	});
 	document.querySelector('.overlay').addEventListener('click', function(){
-		document.querySelector('.gift').classList.toggle('show');
-		document.querySelector('.overlay').classList.toggle('show');
+		document.querySelector('.gift.one').classList.remove('show');
+		document.querySelector('.gift.five').classList.remove('show');
+		document.querySelector('.gift.six').classList.remove('show');
+		document.querySelector('.overlay').classList.remove('show');
 	});
 });
