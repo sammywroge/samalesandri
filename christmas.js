c8d4c35d0be09ae07bffb542a45a2c0a604635a4
sammywroge <99901926+sammywroge@users.noreply.github.com>
2022-12-16 12:59:14 -0600

    updates

--- christmas.js
+++ christmas.js
@@ -4,6 +4,10 @@ document.addEventListener("DOMContentLoaded", function(){
 		document.querySelector('.gift.one').classList.add('show');
 		document.querySelector('.overlay').classList.add('show');
 	});
+	document.querySelector('.christmascountdown>p:nth-child(2)').addEventListener('click', function(){
+		document.querySelector('.gift.two').classList.add('show');
+		document.querySelector('.overlay').classList.add('show');
+	});
 	document.querySelector('.christmascountdown>p:nth-child(5)').addEventListener('click', function(){
 		document.querySelector('.gift.five').classList.add('show');
 		document.querySelector('.overlay').classList.add('show');
@@ -16,6 +20,10 @@ document.addEventListener("DOMContentLoaded", function(){
 		document.querySelector('.gift.one').classList.remove('show');
 		document.querySelector('.overlay').classList.remove('show');
 	});
+	document.querySelector('.gift.two .exit').addEventListener('click', function(){
+		document.querySelector('.gift.two').classList.remove('show');
+		document.querySelector('.overlay').classList.remove('show');
+	});
 	document.querySelector('.gift.five .exit').addEventListener('click', function(){
 		document.querySelector('.gift.five').classList.remove('show');
 		document.querySelector('.overlay').classList.remove('show');
@@ -26,6 +34,7 @@ document.addEventListener("DOMContentLoaded", function(){
 	});
 	document.querySelector('.overlay').addEventListener('click', function(){
 		document.querySelector('.gift.one').classList.remove('show');
+		document.querySelector('.gift.two').classList.remove('show');
 		document.querySelector('.gift.five').classList.remove('show');
 		document.querySelector('.gift.six').classList.remove('show');
 		document.querySelector('.overlay').classList.remove('show');
